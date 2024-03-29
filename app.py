19d3aefe22a4566339f01023cf013861e60ab6f0
Vi-Sri <nikola.tesla195@gmail.com>
2024-03-28 20:13:36 -0400

    project code dump commit 2

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import json 
+import pandas as pd
+from langchain.graphs.neo4j_graph import Neo4jGraph
+from langchain.vectorstores.neo4j_vector import Neo4jVector
+from langchain.embeddings.openai import OpenAIEmbeddings
+from utils import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def set_config(config):
+    config["url"] = "neo4j+s://a27d7ab9.databases.neo4j.io:7687"
+    config["username"] = "neo4j"
+    config["password"] = "UPxug86AK63ZzUQPfLQ4J0MwgD8F5-fhhAgrKxIU6WU"
+    return config
+
+
+if __name__ == "__main__":
+    config = dict()
+    config = set_config(config)
+    graph = Neo4jGraph(url=config["url"],
+                       username=config["username"],
+                       password=config["password"])
+    
+    graph._driver.close()
+    
+    
+
+
+
+
+
+
+
+
